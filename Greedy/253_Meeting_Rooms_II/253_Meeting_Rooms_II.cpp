89688fb1a6095984a3ca3db59385971cb3b8a273
Roberto Strepparava <37817367+rstreppa@users.noreply.github.com>
2023-09-12 21:24:00 -0400

    Create 253_Meeting_Rooms_II.cpp
    Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), find the minimum number of conference rooms required.

---
+++ Greedy/253_Meeting_Rooms_II/253_Meeting_Rooms_II.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+bool compareIntervals(vector<int> a, vector<int> b) {
+    return a[0] < b[0];
+}
+
+int minMeetingRooms(vector<vector<int>>& intervals) {
+    if (intervals.empty()) return 0;
+    
+    // Sort intervals based on start time
+    sort(intervals.begin(), intervals.end(), compareIntervals);
+    
+    // Priority queue to keep track of end times of meetings in rooms
+    priority_queue<int, vector<int>, greater<int>> min_heap;
+    
+    // Initialize by adding the end time of the first meeting
+    min_heap.push(intervals[0][1]);
+    
+    // Iterate through the remaining meetings
+    for (int i = 1; i < intervals.size(); ++i) {
+        // If the earliest ending meeting ends before or exactly when the next meeting starts
+        if (min_heap.top() <= intervals[i][0]) {
+            // Free up the room by removing the earliest ending meeting
+            min_heap.pop();
+        }
+        // Add the ending time of the next meeting into the min heap
+        min_heap.push(intervals[i][1]);
+    }
+    
+    // The size of the min heap will tell us the minimum number of rooms required
+    return min_heap.size();
+}
